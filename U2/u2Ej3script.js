46b08e7fd8076266f4f4f7a4ff18480178a6c8d7
ciclos <ciclos>
2016-09-21 19:34:09 +0200

    Actualizacion

--- U2/u2Ej3script.js
+++ U2/u2Ej3script.js
@@ -1,2 +1,2 @@
-var texto="Las rosas son rojas.\n\t El mar es azul. \n \t No se rimar.";
-document.write(texto);
+var texto="Las rosas son rojas.\n\tEl mar es azul.\n\tNo se rimar.";
+console.log(texto);
